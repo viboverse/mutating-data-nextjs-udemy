4d1c056ad017253742dac817318b88b18b3f9121
vibo <vahab.afsharian94@gmail.com>
2025-09-22 17:32:29 +0300

    Refactor header and posts components to use Next.js Image component for optimized image loading

--- components/posts.js
+++ components/posts.js
@@ -1,18 +1,19 @@
-import { formatDate } from '@/lib/format';
-import LikeButton from './like-icon';
+import { formatDate } from "@/lib/format";
+import LikeButton from "./like-icon";
+import Image from "next/image";
 
 function Post({ post }) {
   return (
     <article className="post">
       <div className="post-image">
-        <img src={post.image} alt={post.title} />
+        <Image src={post.image} alt={post.title} />
       </div>
       <div className="post-content">
         <header>
           <div>
             <h2>{post.title}</h2>
             <p>
-              Shared by {post.userFirstName} on{' '}
+              Shared by {post.userFirstName} on{" "}
               <time dateTime={post.createdAt}>
                 {formatDate(post.createdAt)}
               </time>
